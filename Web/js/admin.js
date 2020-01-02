724141f096c07666eee620d357a6596dda8351bb
NaveenTennakoon <naveentennakoon27@gmail.com>
2020-01-02 22:12:34 +0530

    Current location button

--- Web/js/admin.js
+++ Web/js/admin.js
@@ -810,6 +810,67 @@ function clearOrderModal(){
 // signup page functions
 let map, marker, lat, lng;
 
+function addYourLocationButton(map){
+  let controlDiv = document.createElement('div');
+
+  let firstChild = document.createElement('button');
+  firstChild.style.backgroundColor = '#fff';
+  firstChild.style.border = 'none';
+  firstChild.style.outline = 'none';
+  firstChild.style.width = '28px';
+  firstChild.style.height = '28px';
+  firstChild.style.borderRadius = '2px';
+  firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+  firstChild.style.cursor = 'pointer';
+  firstChild.style.marginRight = '10px';
+  firstChild.style.padding = '0px';
+  firstChild.title = 'Your Location';
+  controlDiv.appendChild(firstChild);
+
+  let secondChild = document.createElement('div');
+  secondChild.style.margin = '5px';
+  secondChild.style.width = '18px';
+  secondChild.style.height = '18px';
+  secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-1x.png)';
+  secondChild.style.backgroundSize = '180px 18px';
+  secondChild.style.backgroundPosition = '0px 0px';
+  secondChild.style.backgroundRepeat = 'no-repeat';
+  secondChild.id = 'you_location_img';
+  firstChild.appendChild(secondChild);
+
+  google.maps.event.addListener(map, 'dragend', function() {
+      $('#you_location_img').css('background-position', '0px 0px');
+  });
+
+  firstChild.addEventListener('click', function() {
+      let imgX = '0';
+      let animationInterval = setInterval(function(){
+          if(imgX == '-18') imgX = '0';
+          else imgX = '-18';
+          $('#you_location_img').css('background-position', imgX+'px 0px');
+      }, 500);
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          let pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          placeMarker(pos, map);  
+          map.setCenter(pos);
+          clearInterval(animationInterval);
+          $('#you_location_img').css('background-position', '0px 0px');
+        });
+      }
+      else{
+        clearInterval(animationInterval);
+        $('#you_location_img').css('background-position', '0px 0px');
+      }
+  });
+
+  controlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
+}
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     streetViewControl: false,
@@ -842,6 +903,7 @@ function initMap() {
       'error'
     )
   }
+  addYourLocationButton(map);
 }
 
 function placeMarker(position, map) {
