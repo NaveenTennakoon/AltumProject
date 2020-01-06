d037cb37821ebbecd8ff9dfb728cccd12b52f46c
NaveenTennakoon <naveentennakoon27@gmail.com>
2020-01-06 23:24:17 +0530

    Map changes and signout authentication

--- Web/js/admin.js
+++ Web/js/admin.js
@@ -386,6 +386,8 @@ function spsnapshotToArray() {
   autocomplete(document.getElementById("spLocations"), spArr);
 };
 
+let history_map;
+
 function loadLocations(){
   let search = document.getElementById("spLocations").value;
   document.getElementById("locations").innerHTML = '';
@@ -394,20 +396,43 @@ function loadLocations(){
       snaphot.forEach(function(childSnapshot){
         document.getElementById("locations").insertAdjacentHTML(
           'beforeend',
-          "<div class='location-item'>"+
-            "<b>Location ID: </b>"+
-            "<span class='location-item-title'>"+childSnapshot.key+"</span><br/><br/>"+
-            "<b class='col-lg-3'>Salesperson ID: </b>"+childSnapshot.val().salesperson+"<br/>"+
-            "<b class='col-lg-3'>Time Stamp: </b>"+childSnapshot.val().timestamp+"<br/>"+
-            "<b class='col-lg-3'>Customer Name: </b>"+childSnapshot.val().customer+"<br/>"+
-            "<b class='col-lg-3'>Shop: </b>"+childSnapshot.val().shopname+"<br/>"+
-            "<b class='col-lg-3'>Address : </b>"+childSnapshot.val().address+"<br/>"+
-            "<button class='btn btn-primary view-location-button float-right' type='button'>View on Map</button><hr class='mt-5'/>"+
-          "</div>"
+          "<div class='row'>"+
+            "<div class='location-item col-xl-6 col-lg-7'>"+
+              "<b>Location ID: </b>"+
+              "<span class='location-item-title'>"+childSnapshot.key+"</span><br/><br/>"+
+              "<b class='col-lg-3'>Salesperson ID: </b>"+childSnapshot.val().salesperson+"<br/>"+
+              "<div style='display: none' class='location-lat'>"+childSnapshot.val().lat+"</div>"+
+              "<div style='display: none' class='location-lng'>"+childSnapshot.val().lng+"</div>"+
+              "<b class='col-lg-3'>Time Stamp: </b>"+childSnapshot.val().timestamp+"<br/>"+
+              "<b class='col-lg-3'>Customer Name: </b>"+childSnapshot.val().customer+"<br/>"+
+              "<b class='col-lg-3'>Shop: </b>"+childSnapshot.val().shopname+"<br/>"+
+              "<b class='col-lg-3'>Address : </b>"+childSnapshot.val().address+"<br/>"+
+            "</div>"+
+            "<div class='location-item-map col-xl-6 col-lg-7'>"+
+              "<div class='h-100' id='map"+childSnapshot.key+"'></div>"+
+            "</div>"+
+          "</div>"+
+          "<hr class='mt-3'/>"
         );
+        history_map = new google.maps.Map(document.getElementById('map'+childSnapshot.key), {
+          streetViewControl: false,
+          mapTypeControl: false,
+          center: {lat: childSnapshot.val().lat, lng: childSnapshot.val().lng},
+          zoom: 9,
+          fullscreenControl: false,
+        });
+        let position = {
+          lat: childSnapshot.val().lat,
+          lng: childSnapshot.val().lng
+        };
+        marker = new google.maps.Marker({
+          icon: {
+            url: './img/marker.png',
+          },
+          position: position,
+          map: history_map
+        });
       })
-    }).then(()=>{
-      locationButtons();
     })
   }
   else{
@@ -424,63 +449,111 @@ function loadLocations(){
         if(childSnapshot.val().salesperson == name){
           document.getElementById("locations").insertAdjacentHTML(
             'beforeend',
-            "<div class='location-item'>"+
-              "<b>Location ID: </b>"+
-              "<span class='location-item-title'>"+childSnapshot.key+"</span><br/><br/>"+
-              "<b class='col-lg-3'>Salesperson ID: </b>"+childSnapshot.val().salesperson+
-              "<b class='ml-5'>Latitude: </b><a class='location-lat'>"+childSnapshot.val().lat+"<a/>"+
-              "<b class='ml-3'>Longitude: </b><a class='location-lng'>"+childSnapshot.val().lng+"</a><br/>"+
-              "<b class='col-lg-3'>Time Stamp: </b>"+childSnapshot.val().timestamp+"<br/>"+
-              "<b class='col-lg-3'>Customer Name: </b>"+childSnapshot.val().customer+"<br/>"+
-              "<b class='col-lg-3'>Shop: </b>"+childSnapshot.val().shopname+"<br/>"+
-              "<b class='col-lg-3'>Address : </b>"+childSnapshot.val().address+"<br/>"+
-              "<button class='btn btn-primary view-location-button float-right' type='button'>View on Map</button><hr class='mt-5'/>"+
-            "</div>"
+            "<div class='row'>"+
+              "<div class='location-item col-xl-6 col-lg-7'>"+
+                "<b>Location ID: </b>"+
+                "<span class='location-item-title'>"+childSnapshot.key+"</span><br/><br/>"+
+                "<b class='col-lg-3'>Salesperson ID: </b>"+childSnapshot.val().salesperson+"<br/>"+
+                "<div style='display: none' class='location-lat'>"+childSnapshot.val().lat+"</div>"+
+                "<div style='display: none' class='location-lng'>"+childSnapshot.val().lng+"</div>"+
+                "<b class='col-lg-3'>Time Stamp: </b>"+childSnapshot.val().timestamp+"<br/>"+
+                "<b class='col-lg-3'>Customer Name: </b>"+childSnapshot.val().customer+"<br/>"+
+                "<b class='col-lg-3'>Shop: </b>"+childSnapshot.val().shopname+"<br/>"+
+                "<b class='col-lg-3'>Address : </b>"+childSnapshot.val().address+"<br/>"+
+              "</div>"+
+              "<div class='location-item-map col-xl-6 col-lg-7'>"+
+                "<div class='h-100' id='map"+childSnapshot.key+"'></div>"+
+              "</div>"+
+            "</div>"+
+            "<hr class='mt-3'/>"
           );
+          history_map = new google.maps.Map(document.getElementById('map'+childSnapshot.key), {
+            streetViewControl: false,
+            mapTypeControl: false,
+            center: {lat: childSnapshot.val().lat, lng: childSnapshot.val().lng},
+            zoom: 9,
+            fullscreenControl: false,
+          });
+          let position = {
+            lat: childSnapshot.val().lat,
+            lng: childSnapshot.val().lng
+          };
+          marker = new google.maps.Marker({
+            icon: {
+              url: './img/marker.png',
+            },
+            position: position,
+            map: history_map
+          });
         }
       })
-    }).then(()=>{
-      locationButtons();
     }) 
   }
 }
 
-function locationButtons() {
-  let viewLocationButtons = document.getElementsByClassName('view-location-button');
-  for (let i = 0; i < viewLocationButtons.length; i++) {
-    let button = viewLocationButtons[i]
-    button.addEventListener('click', viewLocationClicked)
-  }
-}
+let pastMarkers = [];
 
-let history_map;
-
-function viewLocationClicked(event){
-  let button = event.target
-  let locationItem = button.parentElement;
-  let title = locationItem.getElementsByClassName('location-item-title')[0].innerText;
-  let loc_lat = parseFloat(locationItem.getElementsByClassName('location-lat')[0].innerHTML);
-  let loc_lng = parseFloat(locationItem.getElementsByClassName('location-lng')[0].innerHTML);
-  document.getElementById('location-modal-title').innerHTML = title;
-  history_map = new google.maps.Map(document.getElementById('map'), {
+function viewAllLocations(){
+  map = new google.maps.Map(document.getElementById('map'), {
     streetViewControl: false,
     mapTypeControl: false,
-    center: {lat: loc_lat, lng: loc_lng},
-    zoom: 9
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 15
   });
-  let position = {
-    lat: loc_lat,
-    lng: loc_lng
-  };
-  marker = new google.maps.Marker({
-    icon: {
-      url: './img/marker.png',
-    },
-    position: position,
-    map: history_map
+
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      let pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+      map.setCenter(pos);
+    });
+  } 
+  else{
+    Swal.fire(
+      'Something went wrong with location Access',
+      '',
+      'error'
+    )
+  }
+  locationButton(map);
+
+  gpsRef.child('locations').on('child_added', function (snapshot) {
+    addPastLocationMarker(snapshot);
   });
   $('#locationModal').modal({backdrop: 'static', keyboard: false});
   $('#locationModal').modal('show');
+}
+
+function addPastLocationMarker(snapshot){
+  usersRef.child(snapshot.val().salesperson).once("value").then(function(snap){
+    let uluru = { lat: snapshot.val().lat, lng: snapshot.val().lng };
+    let marker = new google.maps.Marker({
+      position: uluru,
+      icon: {
+        url: './img/marker.png',
+      },
+      map: map,
+      title: snapshot.key,
+    });
+    var contentString = '<div id="content">'+
+                          '<div id="siteNotice">'+
+                          '</div>'+
+                          '<p id="name" class="h4">'+snap.val().firstName+" "+snap.val().lastName+"</p>"+
+                          '<div id="bodyContent"><b>'+snapshot.val().shopname+'</b><br/>'+snapshot.val().customer+"<br/>"+snapshot.val().address+
+                          '</div>'+
+                        '</div>';
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map, marker);
+    });
+    pastMarkers[snapshot.key] = marker;
+  });
 }
 
 function refresh(){
@@ -1126,24 +1199,29 @@ function initTrackMap() {
 }
 
 function AddMarker(snapshot) {
-  let uluru = { lat: snapshot.val().lat, lng: snapshot.val().lng };
-  let marker = new google.maps.Marker({
+  usersRef.child(snapshot.key).once("value").then(function(snap){
+    let uluru = { lat: snapshot.val().lat, lng: snapshot.val().lng };
+    let marker = new google.maps.Marker({
       position: uluru,
       icon: {
         url: '../marker.svg',
       },
       map: map,
       title: snapshot.key,
-  });
-  usersRef.child(snapshot.key).once("value").then(function(snap){
+    });
+    var contentString = '<div id="content">'+
+                          '<div id="siteNotice">'+
+                          '</div>'+
+                          '<h1 id="name" class="h6">'+snap.val().firstName+" "+snap.val().lastName+'</h1>'+
+                          '<div id="bodyContent"><b>'+snap.val().telephone+'</b><br/>'+snap.val().address+
+                          '</div>'+
+                        '</div>';
     var infowindow = new google.maps.InfoWindow({
-      content: snap.val().firstName+" "+snap.val().lastName
+      content: contentString
     });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
+    infowindow.open(map, marker);
+    markers[snapshot.key] = marker;
   });
-  markers[snapshot.key] = marker;
 }
 
 function searchClicked(){
--- Web/js/customers.js
+++ Web/js/customers.js
@@ -30,27 +30,51 @@ function logout(){
   });
 }
 
-function signout(){
-  usersRef.child(firebase.auth().currentUser.uid).update({
-    status: "inactive",
-  }).then(() => {
-    firebase.auth().currentUser.delete().then(() => {
-      Swal.fire({
-        position: 'top',
-        icon: 'success',
-        title: 'Thank you for staying with us. See you again',
-        showConfirmButton: false,
-        timer: 3000
+async function signout(){
+  $('#signoutModal').modal('hide');
+  const { value: formValues } = await Swal.fire({
+    title: 'Re-Authenticate user credentials',
+    html:
+      '<input id="swalEmail" class="swal2-input" placeholder="Email address">' +
+      '<input id="swalPassword" class="swal2-input" placeholder="Password" type="password">',
+    focusConfirm: false,
+  }) 
+  if (formValues) {
+    let user = firebase.auth().currentUser;
+    let credential = firebase.auth.EmailAuthProvider.credential(
+      document.getElementById('swalEmail').value,
+      document.getElementById('swalPassword').value
+    );
+    // Prompt the user to re-provide their sign-in credentials
+    user.reauthenticateWithCredential(credential).then(function() {
+      usersRef.child(firebase.auth().currentUser.uid).update({
+        status: "inactive",
+      }).then(() => {
+        firebase.auth().currentUser.delete().then(() => {
+          Swal.fire({
+            position: 'top',
+            icon: 'success',
+            title: 'Thank you for staying with us. See you again',
+            showConfirmButton: false,
+            timer: 3000
+          });
+          location.href = 'index.html';
+        }).catch(function(error){
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: error.message,
+          })
+        })
       });
-      location.href = 'index.html';
-    }).catch(function(error){
+    }).catch(function(error) {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
         text: error.message,
       })
-    })
-  })
+    });
+  }
 }
 
 // Customer dashboard page functions
