675122315143d90939805de079d654672a697173
NaveenTennakoon <naveentennakoon27@gmail.com>
2020-01-23 20:17:14 +0530

    Generating PDFs

--- Web/js/admin.js
+++ Web/js/admin.js
@@ -71,6 +71,8 @@ function dashboard(){
 }
 
 // Reports page functions
+let customerIds = []
+let spIds = []
 function reportsLoad(){
   inventoryRef.once("value").then(function(snapshot){
     snapshot.forEach(function(childSnapshot){
@@ -85,12 +87,104 @@ function reportsLoad(){
       )
     })
   }).then(() => {
-    $(document).ready( function () {
-      $('#reports-product-table').DataTable();
-    });
+    $(document).ready(() => {
+      $('#reports-product-table').DataTable()
+    })
+  })
+  usersRef.once('value').then(function(snapshot){
+    $.map(snapshot.val(), function(value, key){
+      if(value.type == 'customer')
+        customerIds.push({ key: key, id: value.id })
+    })
+  })
+  ordersRef.once("value").then(function(snapshot){
+    snapshot.forEach(function(childSnapshot){
+      let username
+      for(let i = 0; i < customerIds.length; i++){
+        if(customerIds[i].key == childSnapshot.val().Customer)  
+          username = customerIds[i].id
+      }
+      if(childSnapshot.val().Status == 'Cancelled'){
+        document.getElementById("reports-order-body").insertAdjacentHTML(
+          'beforeend',
+          "<tr>"+
+            "<td>"+childSnapshot.val().orderId+"</td>"+
+            "<td>"+username+"</td>"+
+            "<td>"+childSnapshot.val().Total+"</td>"+
+            "<td>"+childSnapshot.val().orderDate+"</td>"+
+            "<td>Order cancelled</td>"+
+            "<td>Order cancelled</td>"+
+            "<td>"+
+              "<table class='table display'>"+
+              "<tbody id='reports-order-body-products"+childSnapshot.key+"'>"
+        )
+      }
+      else{
+        document.getElementById("reports-order-body").insertAdjacentHTML(
+          'beforeend',
+          "<tr>"+
+            "<td>"+childSnapshot.val().orderId+"</td>"+
+            "<td>"+username+"</td>"+
+            "<td>"+childSnapshot.val().Total+"</td>"+
+            "<td>"+childSnapshot.val().orderDate+"</td>"+
+            "<td>"+childSnapshot.val().payment+"</td>"+
+            "<td>"+childSnapshot.val().paymentDate+"</td>"+
+            "<td>"+
+              "<table class='table display'>"+
+              "<tbody id='reports-order-body-products"+childSnapshot.key+"'>"
+        )
+      }
+      $.map(childSnapshot.val().Products, function(value, key){
+        document.getElementById("reports-order-body-products"+childSnapshot.key).insertAdjacentHTML(
+          'beforeend',
+          "<td>"+key+" </td>"+
+          "<td>"+value+" <br/></td>"
+        )
+      })
+      document.getElementById("reports-order-body").insertAdjacentHTML(
+        'beforeend',
+        "</tbody>"+
+        "</table>"+
+        "</td>"+
+        "</tr>"
+      )   
+    })
+  }).then(() => {
+    $(document).ready(() => {
+      $('#reports-order-table').DataTable()
+    })
+  })
+  usersRef.once('value').then(function(snapshot){
+    $.map(snapshot.val(), function(value, key){
+      if(value.type == 'salesperson')
+        spIds.push({ key: key, id: value.id })
+    })
+  })
+  gpsRef.child('locations').once("value").then(function(snapshot){
+    snapshot.forEach(function(childSnapshot){
+      let username
+      for(let i = 0; i < spIds.length; i++){
+        if(spIds[i].key == childSnapshot.val().salesperson)  
+          username = spIds[i].id
+      }
+      document.getElementById("reports-location-body").insertAdjacentHTML(
+        'beforeend',
+        "<tr>"+
+          "<td>"+childSnapshot.val().id+"</td>"+
+          "<td>"+username+"</td>"+
+          "<td>"+childSnapshot.val().customer+"</td>"+
+          "<td>"+childSnapshot.val().shopname+"</td>"+
+          "<td>"+childSnapshot.val().timestamp+"</td>"+
+          "<td>"+childSnapshot.val().address+"</td>"+
+        "</tr>"
+      )
+    })
+  }).then(() => {
+    $(document).ready(() => {
+      $('#reports-location-table').DataTable()
+    })
   })
 }
-
 
 // new salesperson page functions
 function addSalesperson() {
@@ -1337,6 +1431,7 @@ function searchClicked(){
   })
 }
 
+// Payment page functions 
 function loadPaymentDetails(){
   ordersRef.once("value").then(function(snapshot){
     snapshot.forEach(function(childSnapshot){
--- Web/js/jspdf.js
+++ Web/js/jspdf.js
@@ -1,7 +1,21 @@
-let doc = new jsPDF()
-let source = window.document.getElementById('reports-product-table')
-function generatePdf(){
-    doc.text('Hello world!', 10, 10)
-    doc.autoTable({ html: source })
+let doc = new jsPDF({ orientation: 'landscape' })
+let products = window.document.getElementById('reports-product-table')
+let orders = window.document.getElementById('reports-order-table')
+let locations = window.document.getElementById('reports-location-table')
+function generateProductsPdf(){
+    doc.text('Products report', 10, 10)
+    doc.autoTable({ html: products })
+    doc.output("dataurlnewwindow")
+}
+
+function generateOrdersPdf(){
+    doc.text('Orders report', 10, 10)
+    doc.autoTable({ html: orders })
+    doc.output("dataurlnewwindow")
+}
+
+function generateLocationPdf(){
+    doc.text('Locations report', 10, 10)
+    doc.autoTable({ html: locations })
     doc.output("dataurlnewwindow")
 }
